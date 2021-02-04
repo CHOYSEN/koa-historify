50b73a5a340724eec2b9cf38a97b5e068a15a13a
CHOYSEN <choysen@qq.com>
2021-02-04 16:13:33 +0800

    chore: update types

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,5 +1,6 @@
 import type { Middleware } from 'koa';
 interface Options {
+    prepose?: boolean;
     logger?: (msg: string, ...params: any[]) => void;
 }
 declare function koaHistorify(filepath: string, options?: Options): Middleware;
