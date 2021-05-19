3af018a62e18b32e1e0851cb65c93549c3711d14
CHOYSEN <choysen@qq.com>
2021-05-19 15:29:42 +0800

    test: improve test code

--- test/index.test.ts
+++ test/index.test.ts
@@ -5,117 +5,8 @@ import Router from '@koa/router'
 import supertest from 'supertest'
 import koaHistorify from '../src'
 
-let server = null
 const indexPath = path.join(__dirname, 'static/index.html')
 const indexFile = fs.readFileSync(indexPath).toString()
-
-describe('Test request methods:', () => {
-  server = new Koa()
-    .use(koaHistorify(indexPath))
-    .listen()
-
-  it('should not historify when HTTP method is not GET', () => {
-    const methods = ['post', 'put', 'delete', 'head', 'options']
-    methods.forEach(async method => {
-      const res = await supertest(server)[method]('/').set('Accept', 'text/html')
-      expect(res.status).toBe(404)
-      expect(res.text).not.toBe(indexFile)
-    })
-  })
-
-  it('should historify when HTTP request method is GET', async () => {
-    const res = await supertest(server).get('/').set('Accept', 'text/html')
-    expect(res.status).toBe(200)
-    expect(res.text).toBe(indexFile)
-  })
-})
-
-describe('Test request header field "Accept":', () => {
-  server.close()
-  server = new Koa()
-    .use(koaHistorify(indexPath))
-    .listen()
-
-  it('should not historify when HTTP request header field "Accept" does not include "text/html"', async () => {
-    const res = await supertest(server).get('/')
-    expect(res.status).toBe(404)
-    expect(res.text).not.toBe(indexFile)
-  })
-
-  it('should historify when HTTP request header field "Accept" includes "text/html"', async () => {
-    const res = await supertest(server).get('/').set('Accept', 'text/html')
-    expect(res.status).toBe(200)
-    expect(res.text).toBe(indexFile)
-  })
-})
-
-describe('Test ctx.body or ctx.status has been modified:', () => {
-  it("should not historify when ctx.status has been modified", async () => {
-    server.close()
-    server = new Koa()
-      .use(async (ctx, next) => {
-        ctx.status = 204
-        await next()
-      })
-      .use(koaHistorify(indexPath))
-      .listen()
-
-    const res = await supertest(server).get('/api').set('Accept', 'text/html')
-    expect(res.status).toBe(204)
-    expect(res.text).not.toBe(indexFile)
-  })
-
-  it("should not historify when ctx.body having content", async () => {
-    const content = 'ctx.body has been modified'
-    const router = new Router()
-      .get('/api', async (ctx, next) => {
-        ctx.body = content
-        await next()
-      })
-
-    server.close()
-    server = new Koa()
-      .use(router.routes())
-      .use(koaHistorify(indexPath))
-      .listen()
-
-    const res = await supertest(server).get('/api').set('Accept', 'text/html')
-    expect(res.status).toBe(200)
-    expect(res.text).toBe(content)
-  })
-})
-
-describe('Test non-existent routes:', () => {
-  server.close()
-  server = new Koa()
-    .use(koaHistorify(indexPath))
-    .listen()
-
-  it('should historify when request has not been handled', async () => {
-    for (let i = 0; i < 5; i++) {
-      const randomPath = Math.random().toString(36).replace('0.', '/')
-      const res = await supertest(server).get(randomPath).set('Accept', 'text/html')
-      expect(res.status).toBe(200)
-      expect(res.text).toBe(indexFile)
-    }
-  })
-})
-
-describe('Test filepath:', () => {
-  it('should call next() when filepath is wrong', async () => {
-    server.close()
-    server = new Koa()
-      .use(koaHistorify('no'))
-      .use(ctx => {
-        ctx.body = 'content'
-      })
-      .listen()
-
-    const res = await supertest(server).get('/').set('Accept', 'text/html')
-    expect(res.status).toBe(200)
-    expect(res.text).toBe('content')
-  })
-})
 
 describe('Test options:', () => {
   test('"filepath" must be a string', () => {
@@ -135,8 +26,7 @@ describe('Test options:', () => {
   it('should work with a logger', async () => {
     let called = false
 
-    server.close()
-    server = new Koa()
+    const server = new Koa()
       .use(koaHistorify(indexPath, {
         logger() {
           called = true
@@ -146,6 +36,8 @@ describe('Test options:', () => {
 
     await supertest(server).get('/')
     expect(called).toBe(true)
+
+    server.close()
   })
 
   it('should work when prepose mode', async () => {
@@ -156,8 +48,7 @@ describe('Test options:', () => {
         await next()
       })
 
-    server.close()
-    server = new Koa()
+    const server = new Koa()
       .use(koaHistorify(indexPath, { prepose: true }))
       .use(router.routes())
       .listen()
@@ -169,7 +60,121 @@ describe('Test options:', () => {
     res = await supertest(server).get('/api').set('Accept', 'text/html')
     expect(res.status).toBe(200)
     expect(res.text).toBe(content)
+
+    server.close()
   })
 })
 
-afterAll(() => server.close())
+describe('Test request methods:', () => {
+  const server = new Koa()
+    .use(koaHistorify(indexPath))
+    .listen()
+
+  it('should not historify when HTTP method is not GET', async () => {
+    for (const method of ['post', 'put', 'delete', 'head', 'options']) {
+      const res = await supertest(server)[method]('/').set('Accept', 'text/html')
+      expect(res.status).toBe(404)
+      expect(res.text).not.toBe(indexFile)
+    }
+  })
+
+  it('should historify when HTTP request method is GET', async () => {
+    const res = await supertest(server).get('/').set('Accept', 'text/html')
+    expect(res.status).toBe(200)
+    expect(res.text).toBe(indexFile)
+  })
+
+  server.close()
+})
+
+describe('Test request header field "Accept":', () => {
+  const server = new Koa()
+    .use(koaHistorify(indexPath))
+    .listen()
+
+  it('should not historify when HTTP request header field "Accept" does not include "text/html"', async () => {
+    const res = await supertest(server).get('/')
+    expect(res.status).toBe(404)
+    expect(res.text).not.toBe(indexFile)
+  })
+
+  it('should historify when HTTP request header field "Accept" includes "text/html"', async () => {
+    const res = await supertest(server).get('/').set('Accept', 'text/html')
+    expect(res.status).toBe(200)
+    expect(res.text).toBe(indexFile)
+  })
+
+  server.close()
+})
+
+describe('Test ctx.body or ctx.status has been modified:', () => {
+  it("should not historify when ctx.status has been modified", async () => {
+    const server = new Koa()
+      .use(async (ctx, next) => {
+        ctx.status = 204
+        await next()
+      })
+      .use(koaHistorify(indexPath))
+      .listen()
+
+    const res = await supertest(server).get('/api').set('Accept', 'text/html')
+    expect(res.status).toBe(204)
+    expect(res.text).not.toBe(indexFile)
+
+    server.close()
+  })
+
+  it("should not historify when ctx.body has been modified", async () => {
+    const content = 'ctx.body has been modified'
+    const router = new Router()
+      .get('/api', async (ctx, next) => {
+        ctx.body = content
+        await next()
+      })
+
+    const server = new Koa()
+      .use(router.routes())
+      .use(koaHistorify(indexPath))
+      .listen()
+
+    const res = await supertest(server).get('/api').set('Accept', 'text/html')
+    expect(res.status).toBe(200)
+    expect(res.text).toBe(content)
+
+    server.close()
+  })
+})
+
+describe('Test non-existent routes:', () => {
+  const server = new Koa()
+    .use(koaHistorify(indexPath))
+    .listen()
+
+  it('should historify when request has not been handled', async () => {
+    for (let i = 0; i < 5; i++) {
+      const randomPath = Math.random().toString(36).replace('0.', '/')
+      const res = await supertest(server).get(randomPath).set('Accept', 'text/html')
+      expect(res.status).toBe(200)
+      expect(res.text).toBe(indexFile)
+    }
+  })
+
+  server.close()
+})
+
+describe('Test filepath:', () => {
+  const server = new Koa()
+    .use(koaHistorify('no'))
+    .use(ctx => {
+      ctx.body = 'content'
+    })
+    .listen()
+
+  it('should call next() when filepath is wrong', async () => {
+    const res = await supertest(server).get('/').set('Accept', 'text/html')
+    expect(res.status).toBe(200)
+    expect(res.text).toBe('content')
+  })
+
+  server.close()
+})
