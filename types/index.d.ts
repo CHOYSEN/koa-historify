4a2f2dd85b8f8a0c9d37223cdafcde571f22a70f
CHOYSEN <choysen@qq.com>
2021-09-20 00:44:52 +0800

    chore: put the type files into dist folder

--- types/index.d.ts
+++
@@ -1,7 +0,0 @@
-import type { Middleware } from 'koa'
-interface Options {
-  prepose?: boolean
-  logger?: (msg: string) => void
-}
-declare function koaHistorify(filepath: string, options?: Options): Middleware
-export = koaHistorify
